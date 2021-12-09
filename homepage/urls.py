080f108212949f27d188af86b63d5686e6066817
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-12-09 19:20:30 +0600

    Submit Music Page Almost Finished
    - created templates/submit_music.html
    - created static/homepage/css/submit_music.style.css
    - configured submit_music views and urls
    - Finished HTML and CSS

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('homepage/', views.homepage, name='homepage'),
     path('buy-in/', views.buy_in, name='buy_in'),
     path('1-v-1-buy-in/', views.one_vs_one_buy_in, name='one_vs_one_buy_in'),
+    path('submit-music/', views.submit_music, name='submit_music'),
     path('4-tour-producer-chart/', views.four_tour_producer_chart,
          name='four_tour_producer_chart'),
     path('8-tour-producer-chart/', views.eight_tour_producer_chart,
--- homepage/views.py
+++ homepage/views.py
@@ -15,6 +15,10 @@ def one_vs_one_buy_in(request):
     return render(request, '1_v_1_buy_in.html')
 
 
+def submit_music(request):
+    return render(request, 'submit_music.html')
+
+
 def four_tour_producer_chart(request):
     return render(request, '4_tour_producer_chart.html')
 
