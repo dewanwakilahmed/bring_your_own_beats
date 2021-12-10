4152613b817ed6606878744b31a2d4f380e7d08d
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-12-10 16:36:14 +0600

    Login-Register Functionality Done

--- login_register/views.py
+++ login_register/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
 
 def login_register(request):
-    return render(request, 'login_register.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if 'login' in request.POST:
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('/homepage')
+            else:
+                messages.info(request, 'Invalid Credentials!')
+            return redirect('/')
+        elif 'register' in request.POST:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Taken!')
+                return redirect('/')
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password)
+                user.save()
+                messages.info(request, 'Account Registered! Please Login Now')
+            return redirect('/')
+    else:
+        return render(request, 'login_register.html')
